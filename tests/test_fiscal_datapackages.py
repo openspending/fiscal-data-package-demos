e4cc7136dac86347ccb70f9cd5e0950f217998c0
Daniel Fowler <me@danfowler.net>
2015-12-18 12:07:29 +0300

    Change double quotes to single quotes

--- tests/test_fiscal_datapackages.py
+++ tests/test_fiscal_datapackages.py
@@ -6,18 +6,18 @@ import datapackage_validate
 
 class TestFiscalDataPackages(unittest.TestCase):
     def test_datapackage_files_must_be_valid(self):
-        for root, dirs, files in os.walk("."):
+        for root, dirs, files in os.walk('.'):
             for file in files:
-                if file == "datapackage.json":
+                if file == 'datapackage.json':
                     try:
                         with open(os.path.join(root, file), 'r') as f:
                             json.load(f)
                     except ValueError as e:
-                        msg = "File '{0}' isn\'t a valid JSON."
+                        msg = 'File "{0}" isn\'t a valid JSON.'
                         raise ValueError(msg.format(os.path.join(root, file))) from e
 
     def test_datapackage_files_must_be_valid_fiscal_datapackage_files(self):
-        for root, dirs, files in os.walk("."):
+        for root, dirs, files in os.walk('.'):
             for file in files:
                 if file == 'datapackage.json':
                     with open(os.path.join(root, file), 'r') as f:

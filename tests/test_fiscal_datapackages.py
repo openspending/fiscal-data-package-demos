f48076106d03f70ca6e930c412ca9ad85d4ae5cd
Daniel Fowler <me@danfowler.net>
2015-12-18 12:07:08 +0300

    Replace generic validator with  datapackage_validate

--- tests/test_fiscal_datapackages.py
+++ tests/test_fiscal_datapackages.py
@@ -2,7 +2,7 @@ import os
 import glob
 import json
 import unittest
-import jsonschema
+import datapackage_validate
 
 class TestFiscalDataPackages(unittest.TestCase):
     def test_datapackage_files_must_be_valid(self):
@@ -19,14 +19,12 @@ class TestFiscalDataPackages(unittest.TestCase):
     def test_datapackage_files_must_be_valid_fiscal_datapackage_files(self):
         for root, dirs, files in os.walk("."):
             for file in files:
-                if file == "datapackage.json":
-                    schema =  {
-                        "$ref": "https://rawgit.com/dataprotocols/schemas/master/fiscal-data-package.json"
-                    }
+                if file == 'datapackage.json':
                     with open(os.path.join(root, file), 'r') as f:
                         fdp = json.load(f)
                     try:
-                        jsonschema.validate(fdp, schema)
-                    except jsonschema.exceptions.ValidationError as e:
-                        msg = "File '{0}' isn\'t a valid Fiscal Data Package."
-                        raise ValueError(msg.format(os.path.join(root, file))) from e
+                        datapackage_validate.validate(fdp, 'fiscal')
+                    except datapackage_validate.exceptions.DataPackageValidateException as e:
+                        msg = 'File "{0}" isn\'t a valid Fiscal Data Package: {1}'
+                        errs = ','.join([str(err) for err in e.errors])
+                        raise ValueError(msg.format(os.path.join(root, file), errs)) from e
